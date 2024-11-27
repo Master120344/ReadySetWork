864d28ca5db26d86ff5532135e5d087cb92d049a
Master120344 <Master120344@yahoo.com>
2024-11-27 14:12:50 -0600

    Set up Gradle project and initial configuration

---
+++ android/app_backup/src/com/readysetwork/services/NetworkService.java
@@ -0,0 +1,7 @@
+package com.readysetwork.services;
+
+public class NetworkService {
+    public static void fetchDataFromServer() {
+        // Add network call logic here
+    }
+}
---
+++ android/app_backup/src/com/readysetwork/utils/Utility.java
@@ -0,0 +1,9 @@
+nano android/app/src/com/readysetwork/utils/Utility.java
+package com.readysetwork.utils;
+
+public class Utility {
+    public static String formatPhoneNumber(String number) {
+        // Add your phone number formatting logic here
+        return number;
+    }
+}
