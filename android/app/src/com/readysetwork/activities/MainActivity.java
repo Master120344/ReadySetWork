02ad244c0ffc5da7662848f201b89600aff35605
Master120344 <Master120344@yahoo.com>
2024-11-27 13:29:17 -0600

    Add Android activities: MainActivity, LoginActivity, SignupActivity

---
+++ android/app/src/com/readysetwork/activities/MainActivity.java
@@ -0,0 +1,31 @@
+package com.readysetwork.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Navigate to login/signup if necessary
+    }
+}package com.readysetwork.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Navigate to login/signup if necessary
+    }
+}
